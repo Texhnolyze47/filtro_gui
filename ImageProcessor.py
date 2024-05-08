59b9ffbb592bdeb96e7ba859631099725dc91e10
Texhnolyze47 <ichigobankai136@gmail.com>
2024-05-08 09:46:44 -0600

    refactor: functions had to do with image manipulation in their own class were extracted.

---
+++ ImageProcessor.py
@@ -0,0 +1,126 @@
+from tkinter.filedialog import askopenfilename
+from tkinter import messagebox
+import tkinter as tk
+import cv2 as cv
+import numpy as np
+from PIL import ImageTk, Image
+
+
+def read_img(path_image):
+    file_image = cv.imread(path_image)
+    if file_image is None:
+        messagebox.showerror("Error", "Archivo no encontrado")
+        return None
+    file_image = cv.cvtColor(file_image, cv.COLOR_BGR2RGB)
+    return file_image
+
+
+def convert_pil_image(image_path):
+    # Comprueba las dimesiones de un array
+    if image_path.ndim == 2:
+        original_pil_image = Image.fromarray(image_path)
+    else:
+        original_pil_image = Image.fromarray(image_path.astype(np.uint8))
+    original_tk_image = ImageTk.PhotoImage(original_pil_image)
+    return original_tk_image
+
+
+def convert_filtered_image(filtered_path_image):
+    if filtered_path_image.ndim == 2:
+        generic_image = Image.fromarray(np.uint8(filtered_path_image))
+    else:
+        generic_image = Image.fromarray(filtered_path_image[:, :, 0].astype(np.uint8))
+    filtred_tk_image = ImageTk.PhotoImage(generic_image)
+    return filtred_tk_image
+
+
+def display_compare_window(original_image, filtered_image, name_filter):
+    window = tk.Toplevel()
+    window.title(name_filter)
+
+    original_image = convert_pil_image(image_path=original_image)
+
+    orignal_label_image = tk.Label(window, image=original_image)
+    orignal_label_image.pack(side=tk.LEFT)
+
+    filtered_image = convert_filtered_image(filtered_path_image=filtered_image)
+
+    filtered_label_image = tk.Label(window, image=filtered_image)
+    filtered_label_image.pack(side=tk.RIGHT)
+
+    window.mainloop()
+
+
+class ImageProcessor:
+    def __init__(self):
+        self.img_file = []
+
+    def open_file_manager(self):
+        file = askopenfilename(filetypes=(
+            ("File jpg", "*.jpg"),
+            ("File png", "*.png")))
+        if file:
+            try:
+                if self.img_file:
+                    self.img_file[0] = file
+                else:
+                    self.img_file.append(file)
+            except IndexError:
+                messagebox.showerror("Error", "No hay archivo cargado")
+
+    def load_image(self):
+        window = tk.Toplevel()
+        window.geometry("1000x400")
+        window.title("Imagen cargada")
+        path_file = ""
+        try:
+            path_file = self.img_file[0]
+        except IndexError:
+            messagebox.showerror("Error", "No hay un archivo pre cargado")
+        img = Image.open(path_file)
+        tk_image = ImageTk.PhotoImage(img)
+        # TODO: fix - Expected type '_Image | str', got 'PhotoImage' instead
+        label = tk.Label(window, image=tk_image)
+        label.pack()
+        window.mainloop()
+
+    def sobel_filter(self):
+        r = np.matrix("1 2 1; 0 0 0; -1 -2 -1")
+        a = ""
+        try:
+            a = read_img(self.img_file[0])
+        except IndexError:
+            messagebox.showerror("Error", "No hay un archivo cargado")
+        b = np.zeros(a.shape[:3])
+        b[:, :, 0] = cv.filter2D(a[:, :, 0], -1, r)
+
+        display_compare_window(a, b, "sobel_filter")
+
+    def laplacian_filter(self):
+        try:
+            img = read_img(self.img_file[0])
+        except IndexError:
+            messagebox.showerror("Error", "No hay un archivo cargado")
+            return
+
+        # Aplicar el filtro Laplaciano
+        laplacian = cv.Laplacian(img, cv.CV_64F)
+
+        display_compare_window(img, laplacian, "Laplacian_filter")
+
+    def roberts_filter(self):
+
+        try:
+            img = read_img(self.img_file[0])
+        except IndexError:
+            messagebox.showerror("Error", "No hay un archivo cargado")
+            return
+
+        # Convertir la imagen a escala de grises
+        gray = cv.cvtColor(img, cv.COLOR_RGB2GRAY)
+
+        roberts_x = cv.Sobel(gray, cv.CV_64F, 1, 0, ksize=1)
+        roberts_y = cv.Sobel(gray, cv.CV_64F, 0, 1, ksize=1)
+        roberts = np.hypot(roberts_x, roberts_y)
+
+        display_compare_window(img, np.uint8(roberts), "robers_filter")
--- main.py
+++ main.py
@@ -1,144 +1,5 @@
-import cv2 as cv
 import tkinter as tk
-from tkinter.filedialog import askopenfilename
-from tkinter import messagebox
-import numpy as np
-from PIL import ImageTk, Image
-
-img_file = []
-
-
-def read_img(path_image):
-    try:
-        file_image = cv.imread(path_image)
-    except IndexError:
-        messagebox.showerror("Error", "No hay un archivo cargado")
-    file_image = cv.cvtColor(file_image, cv.COLOR_BGR2RGB)
-    return file_image
-
-
-def open_file_manager():
-    archivo = askopenfilename(filetypes=(
-        ("File jpg", "*.jpg"),
-        ("File png", "*.png")))
-    if archivo:
-        try:
-            if img_file:
-                img_file[0] = archivo
-            else:
-                img_file.append(archivo)
-        except IndexError:
-            messagebox.showerror("Error", "No hay un archivo cargado")
-
-
-def load_image():
-    window = tk.Toplevel()
-    window.geometry("1000x400")
-    window.title("Imagen cargada")
-    path_file = ""
-    try:
-        path_file = img_file[0]
-    except IndexError:
-        messagebox.showerror("Error", "No hay un archivo pre cargado")
-    img = Image.open(path_file)
-    tk_image = ImageTk.PhotoImage(img)
-    # TODO: fix - Expected type '_Image | str', got 'PhotoImage' instead
-    label = tk.Label(window, image=tk_image)
-    label.pack()
-    window.mainloop()
-
-
-def sobel_filter():
-    window = tk.Toplevel()
-    window.title("Filtro sobel")
-
-    r = np.matrix("1 2 1; 0 0 0; -1 -2 -1")
-    a = ""
-    try:
-        a = read_img(img_file[0])
-    except IndexError:
-        messagebox.showerror("Error", "No hay un archivo cargado")
-    b = np.zeros(a.shape[:3])
-    b[:, :, 0] = cv.filter2D(a[:, :, 0], -1, r)
-
-    original_pil_image = Image.fromarray(a.astype(np.uint8))
-    original_tk_image = ImageTk.PhotoImage(original_pil_image)
-
-    orignal_label_image = tk.Label(window, image=original_tk_image)
-    orignal_label_image.pack(side=tk.LEFT)
-
-    filtred_pil_image = Image.fromarray(b[:, :, 0].astype(np.uint8))
-    filtred_tk_image = ImageTk.PhotoImage(filtred_pil_image)
-
-    filtered_label_image = tk.Label(window, image=filtred_tk_image)
-    filtered_label_image.pack(side=tk.RIGHT)
-
-    window.mainloop()
-
-
-def laplacian_filter():
-    window = tk.Toplevel()
-    window.title("Filtro Laplaciano")
-
-    try:
-        img = read_img(img_file[0])
-    except IndexError:
-        messagebox.showerror("Error", "No hay un archivo cargado")
-        return
-
-    # Aplicar el filtro Laplaciano
-    laplacian = cv.Laplacian(img, cv.CV_64F)
-
-    original_pil_image = Image.fromarray(img.astype(np.uint8))
-    original_tk_image = ImageTk.PhotoImage(original_pil_image)
-
-    orignal_label_image = tk.Label(window, image=original_tk_image)
-    orignal_label_image.pack(side=tk.LEFT)
-
-    # Convertir la imagen procesada a formato PIL para mostrarla en Tkinter
-    laplacian_pil_image = Image.fromarray(laplacian.astype(np.uint8))
-    laplacian_tk_image = ImageTk.PhotoImage(laplacian_pil_image)
-
-    # Crear una etiqueta para mostrar la imagen
-    laplacian_label_image = tk.Label(window, image=laplacian_tk_image)
-    laplacian_label_image.pack(side=tk.RIGHT)
-
-
-
-    window.mainloop()
-
-
-def roberts_filter():
-    window = tk.Toplevel()
-    window.title("Filtro Roberts")
-
-    try:
-        img = read_img(img_file[0])
-    except IndexError:
-        messagebox.showerror("Error", "No hay un archivo cargado")
-        return
-
-    # Convertir la imagen a escala de grises
-    gray = cv.cvtColor(img, cv.COLOR_RGB2GRAY)
-
-    roberts_x = cv.Sobel(gray, cv.CV_64F, 1, 0, ksize=1)
-    roberts_y = cv.Sobel(gray, cv.CV_64F, 0, 1, ksize=1)
-    roberts = np.hypot(roberts_x, roberts_y)
-
-    original_pil_image = Image.fromarray(img)
-    original_tk_image = ImageTk.PhotoImage(original_pil_image)
-
-    orignal_label_image = tk.Label(window, image=original_tk_image)
-    orignal_label_image.pack(side=tk.LEFT)
-
-    roberts_pil_image = Image.fromarray(np.uint8(roberts))
-    roberts_tk_image = ImageTk.PhotoImage(roberts_pil_image)
-
-    roberts_label_image = tk.Label(window, image=roberts_tk_image)
-    roberts_label_image.pack(side=tk.RIGHT)
-
-    window.mainloop()
-
+from ImageProcessor import ImageProcessor
 
 if __name__ == "__main__":
     root = tk.Tk()
@@ -146,20 +7,22 @@ if __name__ == "__main__":
     root.minsize(600, 370)
     root.title("PDI: PROCESAMIENTO DIGITAL DE IMAGENES")
 
+    image_processor = ImageProcessor()
+
     root.columnconfigure((0, 1), weight=1, uniform="a")
     barraMenu = tk.Menu(root)
     menuArchivo = tk.Menu(barraMenu)
-    menuArchivo.add_command(label="Subir imagen", command=open_file_manager)
-    menuArchivo.add_command(label="Imagenes precargadas", command=load_image)
+    menuArchivo.add_command(label="Subir imagen", command=image_processor.open_file_manager)
+    menuArchivo.add_command(label="Imagenes precargadas", command=image_processor.load_image)
     menuArchivo.add_separator()
     menuArchivo.add_command(label="Exit")
     barraMenu.add_cascade(label="Imagenes", menu=menuArchivo)
 
     feMenu = tk.Menu(barraMenu)
     barraMenu.add_cascade(label="Filtro Espacial", menu=feMenu)
-    feMenu.add_command(label="Sobel", command=sobel_filter)
-    feMenu.add_command(label="Laplaciano", command=laplacian_filter)
-    feMenu.add_command(label="roberts", command=roberts_filter)
+    feMenu.add_command(label="Sobel", command=image_processor.sobel_filter)
+    feMenu.add_command(label="Laplaciano", command=image_processor.laplacian_filter)
+    feMenu.add_command(label="roberts", command=image_processor.roberts_filter)
 
     ffMenu = tk.Menu(barraMenu)
     barraMenu.add_cascade(label="Filtro Frecuencia", menu=ffMenu)
