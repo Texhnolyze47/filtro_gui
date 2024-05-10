88036fe9603c1f669f04f16b4ad02395b9d0d28a
Texhnolyze47 <ichigobankai136@gmail.com>
2024-05-09 20:47:56 -0600

    the variable names have been changed for better understanding

--- ImageProcessor.py
+++ ImageProcessor.py
@@ -9,44 +9,44 @@ from PIL import ImageTk, Image
 
 
 def read_img(path_image):
-    file_image = cv.imread(path_image)
-    if file_image is None:
+    input_image = cv.imread(path_image)
+    if input_image is None:
         messagebox.showerror("Error", "Archivo no encontrado")
         return None
-    file_image = cv.cvtColor(file_image, cv.COLOR_BGR2RGB)
-    return file_image
+    input_image = cv.cvtColor(input_image, cv.COLOR_BGR2RGB)
+    return input_image
 
 
-def convert_pil_image(image_path):
-    # Comprueba las dimesiones de un array
-    if image_path.ndim == 2:
-        original_pil_image = Image.fromarray(image_path)
+def convert_pil_image(original_image_array):
+    # Comprueba las dimensiones de un array
+    if original_image_array.ndim == 2:
+        original_pil_image = Image.fromarray(original_image_array)
     else:
-        original_pil_image = Image.fromarray(image_path.astype(np.uint8))
+        original_pil_image = Image.fromarray(original_image_array.astype(np.uint8))
     original_tk_image = ImageTk.PhotoImage(original_pil_image)
     return original_tk_image
 
 
-def convert_filtered_image(filtered_path_image):
-    if filtered_path_image.ndim == 2:
-        generic_image = Image.fromarray(np.uint8(filtered_path_image))
+def convert_filtered_image(filtered_image_array):
+    if filtered_image_array.ndim == 2:
+        generic_image = Image.fromarray(np.uint8(filtered_image_array))
     else:
-        generic_image = Image.fromarray(filtered_path_image[:, :, 0].astype(np.uint8))
+        generic_image = Image.fromarray(filtered_image_array[:, :, 0].astype(np.uint8))
 
     filtred_tk_image = ImageTk.PhotoImage(generic_image)
     return filtred_tk_image
 
 
-def display_compare_window(original_image, filtered_image, name_filter):
+def display_compare_window(original_image, filtered_image, filter_name):
     window = tk.Toplevel()
-    window.title(name_filter)
+    window.title(filter_name)
 
-    original_image = convert_pil_image(image_path=original_image)
+    original_image = convert_pil_image(original_image_array=original_image)
 
     orignal_label_image = tk.Label(window, image=original_image)
     orignal_label_image.pack(side=tk.LEFT)
 
-    filtered_image = convert_filtered_image(filtered_path_image=filtered_image)
+    filtered_image = convert_filtered_image(filtered_image_array=filtered_image)
 
     filtered_label_image = tk.Label(window, image=filtered_image)
     filtered_label_image.pack(side=tk.RIGHT)
@@ -56,7 +56,7 @@ def display_compare_window(original_image, filtered_image, name_filter):
 
 class ImageProcessor:
     def __init__(self):
-        self.img_file = []
+        self.img_files = []
 
     def open_file_manager(self):
         file = askopenfilename(filetypes=(
@@ -64,10 +64,10 @@ class ImageProcessor:
             ("File png", "*.png")))
         if file:
             try:
-                if self.img_file:
-                    self.img_file[0] = file
+                if self.img_files:
+                    self.img_files[0] = file
                 else:
-                    self.img_file.append(file)
+                    self.img_files.append(file)
             except IndexError:
                 messagebox.showerror("Error", "No hay archivo cargado")
 
@@ -77,19 +77,20 @@ class ImageProcessor:
         window.title("Imagen cargada")
         path_file = ""
         try:
-            path_file = self.img_file[0]
+            path_file = self.img_files[0]
         except IndexError:
             messagebox.showerror("Error", "No hay un archivo pre cargado")
         img = Image.open(path_file)
         tk_image = ImageTk.PhotoImage(img)
         # TODO: fix - Expected type '_Image | str', got 'PhotoImage' instead
         label = tk.Label(window, image=tk_image)
+
         label.pack()
         window.mainloop()
 
     def get_image(self):
         try:
-            return read_img(self.img_file[0])
+            return read_img(self.img_files[0])
         except IndexError:
             messagebox.showerror("Error", "No hay un archivo cargado")
             return None
